e5db56779e32eabb05244468b9fdc5ffc91740bd
Student <example@example.com>
2020-02-17 15:53:15 -0800

    test to create individual water intake function passes

--- __tests__/water-intake-calculator.test.js
+++ __tests__/water-intake-calculator.test.js
@@ -9,12 +9,19 @@ describe("IndividualWaterIntake", () => {
 		})
 		
 	test("should create an object based on user input of age, gender OR height and weight, caffeine intake, activity level and zip code of current location", () => {
+		individualWaterIntake.age = 18;
+		individualWaterIntake.gender = "female";
+		individualWaterIntake.height = 65;
+		individualWaterIntake.weight = 150;
+		individualWaterIntake.caffeineIntake = 4;
+		individualWaterIntake.activity = true;
+		individualWaterIntake.location = 92346;
 		expect(individualWaterIntake.age).toEqual(18);
-		expect(individualWaterIntake.gender).toEqual(female);
+		expect(individualWaterIntake.gender).toEqual("female");
 		expect(individualWaterIntake.height).toEqual(65);
 		expect(individualWaterIntake.weight).toEqual(150);
 		expect(individualWaterIntake.caffeineIntake).toEqual(4);
 		expect(individualWaterIntake.activity).toEqual(true);
 		expect(individualWaterIntake.location).toEqual(92346);
 	}); 
-});ag
+});
