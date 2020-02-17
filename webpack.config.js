d3fc8705d52b51d1d5d4d394932dfb5b08a16dbd
Student <example@example.com>
2020-02-17 09:14:58 -0800

    create project environment

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'PROJECT-TITLE',
+      title: 'water-tracker',
       template: './src/index.html',
       inject: 'body'
     }), 
