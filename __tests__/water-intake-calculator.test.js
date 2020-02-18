c1b5d4dca03340d0a9719081b1974ef2f11efbc5
Student <example@example.com>
2020-02-18 13:54:33 -0800

    update water-intake-calcs. change temperature units to kelvin.

--- __tests__/water-intake-calculator.test.js
+++ __tests__/water-intake-calculator.test.js
@@ -13,25 +13,28 @@ describe("IndividualWaterIntake", () => {
 		individualWaterIntake.weight = 150;
 		individualWaterIntake.caffeineIntake = 4;
 		individualWaterIntake.activity = true;
-		individualWaterIntake.location = 92346;
+		individualWaterIntake.city = "Portland";
+		individualWaterIntake.state = "OR";
 		expect(individualWaterIntake.age).toEqual(18);
 		expect(individualWaterIntake.gender).toEqual("female");
 		expect(individualWaterIntake.height).toEqual(65);
 		expect(individualWaterIntake.weight).toEqual(150);
 		expect(individualWaterIntake.caffeineIntake).toEqual(4);
 		expect(individualWaterIntake.activity).toEqual(true);
-		expect(individualWaterIntake.location).toEqual(92346);
+		expect(individualWaterIntake.city).toEqual("Portland");
+		expect(individualWaterIntake.state).toEqual("OR");
 	}); 
 
 	test("should create a method that adds user input to IndividualWaterIntake object", () => {
-		individualWaterIntake.addUserInput(55, "male", 70, 200, 1, false, 90210);
+		individualWaterIntake.addUserInput(55, "male", 70, 200, 1, false, "Los Angeles", "CA");
 		expect(individualWaterIntake.age).toEqual(55);
 		expect(individualWaterIntake.gender).toEqual("male");
 		expect(individualWaterIntake.height).toEqual(70);
 		expect(individualWaterIntake.weight).toEqual(200);
 		expect(individualWaterIntake.caffeineIntake).toEqual(1);
 		expect(individualWaterIntake.activity).toEqual(false);
-		expect(individualWaterIntake.location).toEqual(90210);
+		expect(individualWaterIntake.city).toEqual("Los Angeles");
+		expect(individualWaterIntake.state).toEqual("CA");
 	});
 
 	test("should calculate user bmi", () => {
@@ -42,14 +45,14 @@ describe("IndividualWaterIntake", () => {
 	});  
 	
 	test("should calculate hydration goal", () => {
-		individualWaterIntake.gender = false;
+		individualWaterIntake.gender = '';
 		individualWaterIntake.height = 67;
 		individualWaterIntake.weight = 180;
 		individualWaterIntake.age = 25; 
 		individualWaterIntake.caffeineIntake = 3;
 		individualWaterIntake.activity = true; 		
-		individualWaterIntake.temperature = 90;
-		individualWaterIntake.humidity = 75;
+		individualWaterIntake.temperature = 306;
+		individualWaterIntake.humidity = 25;
 		individualWaterIntake.calculateHydrationGoal();
 		expect(individualWaterIntake.hydrationGoal).toEqual(16);
 	});
